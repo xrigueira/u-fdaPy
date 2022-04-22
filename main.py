f6a8beeea9beca69029a0ad3866b81134195e550
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2022-04-22 15:37:41 +0200

    Gap checker

---
+++ main.py
@@ -0,0 +1,6 @@
+
+from checkJumps import checkJumps
+
+if __name__ == '__main__':
+    
+    checkJumps('Amonio.txt')
--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-import skfda as fda
-from matplotlib import pyplot as plt
-
-X, y = fda.datasets.fetch_weather(return_X_y=True, as_frame=True)
-
-fd = X.iloc[:, 0].values
-
-print(fd)
-
-msplot = fda.exploratory.visualization.MagnitudeShapePlot(fd, multivariate_depth = fda.exploratory.depth.multivariate.SimplicialDepth())
-
-msplot.plot()
-
-plt.show()
+def foo(text):
+    print(text)

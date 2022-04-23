fdb2311fdc8c9cc4dc3de8423802a86edb09f987
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2022-04-23 12:41:39 +0200

    normalizer_v1.1

--- normalizer.py
+++ normalizer.py
@@ -22,57 +22,44 @@ df['hour'] = hour
 df['minute'] = minute
 df['second'] = second
 
+# Save temp file
+df.index.name = 'date'
+df.to_csv(f'Database/{fileName}_temp.csv', sep=';', encoding='utf-8', index=True, header=['value', 'year', 'month', 'day', 'hour', 'minute', 'second'])
+
 # Add the 31st day to those months with 30
+df = pd.read_csv(f'Database/{fileName}_temp.csv', delimiter=';', parse_dates=['date'])
 monthShort = [i + 1 for (x, y, i) in zip(day, day[1:], range(len(day))) if (x - y) == 29]
 print('These are the last indexes of the months with 30 days: ', monthShort)
 
-startDate =  df.iloc[monthShort[0]-1].name # En el while loop el [0] se deja tal y como está porque siempre cogemos el primer valor despues de actualizar la lista
-yearInit, monthInit, dayInit, hourInit, minInit, secInit = startDate.year, startDate.month, startDate.day + 1, 0, 0, 0
-rows = [[f'{yearInit}-{monthInit}-{dayInit} 00:00:00', np.nan, yearInit, monthInit, dayInit, hourInit, minInit, secInit]]
+counter = 0
+while monthShort:
 
-for i in range(95):
+    for i in monthShort:
 
-    minInit += 15
+        if df['day'][i-1] == 30:
+            
+            startDate =  df.iloc[monthShort[0]-1, 0] 
+            yearInit, monthInit, dayInit, hourInit, minInit, secInit = startDate.year, startDate.month, startDate.day + 1, 0, 0, 0
+            rows = [[f'{yearInit}-{monthInit}-{dayInit} 00:00:00', np.nan, yearInit, monthInit, dayInit, hourInit, minInit, secInit]]
 
-    if minInit > 45:
+            for j in range(95):
 
-        minInit = 0
+                minInit += 15
 
-        hourInit = hourInit + 1
-    
-    rows.append([f'{yearInit}-{monthInit}-{dayInit} {hourInit}:{minInit}:00', np.nan, yearInit, monthInit, dayInit, hourInit, minInit, secInit])
+                if minInit > 45:
 
-# Meter desde la fila 29 hasta aquí en el while loop
-# https://www.adamsmith.haus/python/answers/how-to-build-a-pandas-dataframe-with-a-for-loop-in-python
+                    minInit = 0
 
-# while monthShort:
+                    hourInit = hourInit + 1
+                
+                rows.append([f'{yearInit}-{monthInit}-{dayInit} {hourInit}:{minInit}:00', np.nan, yearInit, monthInit, dayInit, hourInit, minInit, secInit])
 
-#     for i in monthShort:
+            df2concat = pd.DataFrame(rows, columns=['date', 'value', 'year', 'month', 'day', 'hour', 'minute', 'second'])
 
-#         if df['day'][i-1] == 30:
+            df = pd.concat([df.iloc[:i], df2concat, df.iloc[i:]], ignore_index=True)
 
-#             df2concat = pd.DataFrame()
+            day = df['day']
+            
+            monthShort = [i + 1 for (x, y, i) in zip(day, day[1:], range(len(day))) if (x - y) == 29]
 
-#             df = pd.concat([df.iloc[:i], df2concat, df.iloc[i:]], ignore_index=True)
-
-#             day = df['day']
-
-#             monthShort = [i + 1 for (x, y, i) in zip(day, day[1:], range(len(day))) if (x - y) == 29]
-
-#             print('UPDATED indexes of months with 30 days: ', monthShort)
-
-# minute = 00
-# hour = 00
-
-# for i in range(288):
-    
-#     time = f'{hour}:{minute}'
-    
-#     print(time)
-    
-#     if minute == 55:
-#         hour = hour + 1
-#         minute = 0
-#     else:
-#         minute = minute + 5
-
+            print('UPDATED indexes of months with 30 days: ', monthShort)
